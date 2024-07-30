52358e9ba52d125e75c8a6055ddfb78fd37c4e8b
LUCAS CARVALHO RESENDE <38538475+lucasresende93@users.noreply.github.com>
2024-07-30 17:43:03 -0300

    refactor input style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,22 @@ const Page = () => {
 
   return (
     <div className="w-screen h-screen  flex flex-col junstify-center items-center text-3xl">
-      <input type="text" className="w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite uma nova tarefa" value={task} onChange={(e) => setTask(e.target.value)} />
+      <input type="text" 
+      className="w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm 
+      rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+      dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+      dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+      placeholder="Digite uma nova tarefa" 
+      value={task} 
+      onChange={(e) => setTask(e.target.value)} 
+      />
 
       <button className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
        focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
-       focus:outline-none dark:focus:ring-blue-800" onClick={handleAddTask}>Adicionar</button>
+       focus:outline-none dark:focus:ring-blue-800" 
+       onClick={handleAddTask}>
+        Adicionar
+        </button>
 
       <p>{list.length} Itens na Lista:</p>
 
